364c4f41c5dd9086b0e55c22ed0b7ab8ee4d53ef
Mohammad Noureddine <noureddi@rose-hulman.edu>
2025-02-04 12:39:38 -0500

    fixed issue with dependencies on the client and the missing services on the server

--- volumes/fix_depends.sh
+++ volumes/fix_depends.sh
@@ -2,6 +2,7 @@
 
 chmod 1777 /tmp &&
   apt-key add /volumes/archive-key.asc &&
-  apt update --fix-missing &&
-  echo "N" | apt install -y wget sudo curl
+  apt update --fix-missing > /dev/null 2>&1 &&
+  echo "N" | apt install -y wget sudo curl > /dev/null 2>&1 &&
+  echo "Fixing dependencies done..."
 
